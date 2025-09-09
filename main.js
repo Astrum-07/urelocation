6771c2a75b272b36e78835a3f2093a708eb50a00
Astrum-07 <astrumastro07@gmail.com>
2025-09-10 00:52:20 +0500

    hi bro

--- main.js
+++ main.js
@@ -1,1 +1,126 @@
+// 1-misol
+// let num = +prompt("Son kiriting");
 
+// if (num > 0) {
+//   console.log(num + 1);
+// } else {
+//   console.log(num);
+// }
+
+// 2-misol
+// let num = +prompt("Son kiriting");
+
+// if (num > 0) {
+//   console.log(num + 1);
+// } else if (num < 0) {
+//   console.log(num - 2);
+// } else {
+//   console.log(num)=("xatolik");
+// }
+
+// 3-misol
+// let num = +prompt("Son kiriting");
+// let on = 10;
+// if (num > 0) {
+//   console.log(num + 1);
+// } else if (num < 0) {
+//   console.log(num - 2);
+// } else if (num = 0) {
+//   console.log(on);
+// } else {
+//   console.log(10);
+// }
+
+// 4-misol
+// let a = (prompt("1-son"));
+// let b = (prompt("2-son"));
+// let c =(prompt("3-son"));
+
+// let count = (a > 0) + (b > 0) + (c > 0);
+
+// console.log("Musbat sonlar soni: " + count);
+
+// 5-misol
+// let a = (prompt("1-son"));
+// let b = (prompt("2-son"));
+// let c =(prompt("3-son"));
+
+// let count = (a > 0) + (b > 0) + (c > 0);
+// let countt = (a < 0) + (b < 0) + (c < 0);
+
+// console.log("Musbat sonlar soni: " + count);
+// console.log("Muanfiy sonlar soni: " + countt);
+
+// 6-misol
+// let a = prompt("1-son");
+// let b = prompt("2-son");
+// if (a > b) {
+//   console.log( a);
+// } else if (a < b) {
+//   console.log( b);
+// } else {
+//   console.log("teng");
+// }
+
+// 7-misol
+// let a = prompt("1-son");
+// let b = prompt("2-son");
+// if (a > b) {
+//   console.log("kichigi"+ b);
+// } else if (a < b) {
+//   console.log("kichigi"+ a);
+// } else {
+//   console.log("teng");
+// }
+
+// 8-misol
+// let a = prompt("1-son");
+// let b = prompt("2-son");
+
+// if (a > b) {
+//   console.log("kattasi"+ a);
+// } else if (a < b) {
+//   console.log("kattasi"+ b);
+// } else {
+//   console.log("teng");
+// }
+
+// if (a > b) {
+//   console.log("kichigi"+ b);
+// } else if (a < b) {
+//   console.log("kichigi"+ a);
+// } else {
+//   console.log("teng");
+// }
+
+// 9-misol
+// let a = +prompt("A-son");
+// let b = +prompt("B-son");
+// if (a > b) {
+//   console.log(a - b);
+// } else if (a < b) {
+//   console.log(a);
+// }else{
+//     console.log("xatolik")
+// }
+
+// if (a < b) {
+//   console.log(b);
+// } else if (a > b) {
+//   console.log(b + a);
+// }else{
+//     console.log("xatolik")
+// }
+
+// 10-misol
+// let a = +prompt("A-son");
+// let b = +prompt("B-son");
+// let yig = a + b;
+// let nol = 0;
+// if (a > b) {
+//   console.log(yig);
+// }else if(a<b){
+//   console.log(yig);
+// }else{
+//     console.log(nol)
+// }
